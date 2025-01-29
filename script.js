86cd029f402c4b73301db823dab123cf967ff6be
Joy Mich <179646389+DevMichcodes@users.noreply.github.com>
2025-01-29 13:39:46 +0300

    Disable inputs after Game over
    Ensures no more inputs are accepted once the player
    quits the game for accurate score results

--- script.js
+++ script.js
@@ -1,161 +1,10 @@
-
-
-/*let computer_score =0;
-let player_score =0;
-
-// getting computer choice for the game
-function getComputerChoice(){
-    let computer_choice = ["Rock","Paper","Scissors"];
-        return computer_choice[Math.floor(Math.random ()*3)];
-}
-
-//console.log(getComputerChoice());
-// playing a single round
-let player_choice ="";
-const choices =["rock","paper","scissors","quit"];
-
-function getPlayerChoice(player_choice){
-    const playerChose = document.getElementById("playerChoice");
-    playerChose.textContent = player_choice;
-    /*player_choice = prompt("Enter your choice:");
-    if (player_choice == null){
-        return "quit";
-    }*/
-  /*  return player_choice.toLowerCase();
-}
-//console.log(getComputerChoice(),getPlayerChoice());
-
-// playing a round
-function playRound(){
-    let computer_choice = getComputerChoice().toLowerCase();
-   //return both player and computer choices
-    console.log(`Player choice: ${player_choice}`);
-    console.log(`Computer choice: ${computer_choice}`);
-// logic to get winner
-    if (!choices.includes(player_choice)){
-        return "Invalid choice, choose either rock, paper or scissors";
-    }
-    if(player_choice == computer_choice){
-        return "It is a tie!";
-    }
-    if((player_choice == "scissors" && computer_choice == "paper")||
-       (player_choice == "rock" && computer_choice == "scissors")||
-       (player_choice == "paper" && computer_choice == "rock")){
-        player_score ++;
-       return `You win!,${player_choice} beats ${computer_choice} `;
-       
-    }else{
-        computer_score++;
-        return `You lose! ${computer_choice} beats ${player_choice}`;
-        
-    }
-    
-
-}
-
-//console.log(getPlayerChoice(), getComputerChoice());// get choices
-
-//for game over
-/*function gameOver(){
-    if(player_score > computer_score){
-        return "Congratulations! You win!";
-    }else if(player_choice == "quit"){
-        return "Game over";
-    }
-   
-    else if (player_score == computer_score){
-        return "It's A Draw!";
-    }else{
-        return "You lose! Try Again"
-    }
-    
-}
-
-while(true){
-    let player_choice = getPlayerChoice();
-
-    if (!choices.includes(player_choice)) {
-        console.log("Invalid choice. Please enter rock, paper, scissors, or quit.");
-        continue;
-    }
-    if(player_choice == "quit"){
-    break;
-    }
-
-    let computer_choice = getComputerChoice();
-    let result = playRound(player_choice,computer_choice);
-    console.log(result);
-    console.log(`player score:${player_score} computer score: ${computer_score}`);
-}
-console.log(gameOver());*/
-
-// event listeners
-
-/*const buttons = document.querySelectorAll("button");
-buttons.forEach((button)=> {
-    button.addEventListener("click",(event)=>{
-       const choice = event.target.getAttribute("data-choice");
-//selects where the result will be displayed
-       const gameResult = document.getElementById("results");
-       if (choice === "quit") {
-        document.getElementById("results").textContent = "Game Over!";
-        return;
-    }else{
-        document.getElementById("results").textContent = `You selected : ${choice}`;
-    
- }
-console.log("Before updating the DOM");
-const computer_choice = getComputerChoice();
-const result = playRound(player_choice, computer_choice);
-
-
-
-
-    
-      
-})
-    
-})*/
-// updating the UI
-/*
-function showResults(player_choice,computer_choice,result){
-            //update choices
-           document.getElementById("playerChoice").textContent = player_choice;
-           document.getElementById("computerChoice").textContent = computer_choice; 
-
-            // Update Scores
-            document.getElementById("player-score").textContent = player_score;
-            document.getElementById("computer-score").textContent = computer_score;
-
-            // Display Result
-            document.getElementById("results").textContent = result;
-}
-
-//add event listeners
-
-const buttons = document.querySelectorAll("button");
-    buttons.forEach((button) =>{
-        button.addEventListener("click",(event)=>{
-            const choice = event.target.getAttribute("data-choice");
-            if(player_choice === "quit"){
-                document.getElementById("results").textContent = "Game over!";
-                return;
-            }
-            const computer_choice = getComputerChoice();
-            const result = playRound(player_choice,computer_choice);
-            showResults(player_choice,computer_choice,result);
-
-        });
-
-    });
-    */
-    
 // initialising player scores
 
 let player_score = 0;
 let computer_score = 0;
 const player_choice ="";
 const computer_choice ="";
+let gameOver = false;
 
 function getComputerChoice(){
     let computer_choice = ["Rock","Paper","Scissors"];
@@ -163,7 +12,7 @@ function getComputerChoice(){
 }
 
 function getPlayerChoice(event){
-    
+    if(gameOver) return;//no accepting inputs if game over
     const player_choice = event.target.dataset.choice;
     
     const computer_choice =getComputerChoice();
@@ -198,6 +47,8 @@ else if(
 
 
 function quitGame(){
+    gameOver = true;
+    
     let showResult = 'GAME OVER!';
     if(player_score > computer_score){
         showResult += 'You win!';
